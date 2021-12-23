9e0c9a071897a41d4804a5a59f4a707e91c540bd
LvGJ <lvgj1998@qq.com>
2021-12-23 20:25:15 +0800

    Change makefile

--- pkg/yoda/scheduler.go
+++ pkg/yoda/scheduler.go
@@ -2,16 +2,12 @@ package yoda
 
 import (
 	"context"
-	"errors"
-	v1 "k8s.io/api/core/v1"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
-
-	scv "github.com/NJUPT-ISL/SCV/api/v1"
 )
 
 const (
@@ -28,23 +24,6 @@ var (
 
 type Yoda struct {
 	handle framework.Handle
-	cache  cache.Cache
-}
-
-func (y *Yoda) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
-	panic("implement me")
-}
-
-func (y *Yoda) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-	panic("implement me")
-}
-
-func (y *Yoda) PreFilter(ctx context.Context, state *framework.CycleState, p *v1.Pod) *framework.Status {
-	panic("implement me")
-}
-
-func (y *Yoda) PreFilterExtensions() framework.PreFilterExtensions {
-	panic("implement me")
 }
 
 func (y *Yoda) Name() string {
@@ -55,11 +34,6 @@ func New(_ runtime.Object, h framework.Handle) (framework.Plugin, error) {
 	mgrConfig := ctrl.GetConfigOrDie()
 	mgrConfig.QPS = 1000
 	mgrConfig.Burst = 1000
-
-	if err := scv.AddToScheme(scheme); err != nil {
-		klog.Error(err)
-		return nil, err
-	}
 
 	mgr, err := ctrl.NewManager(mgrConfig, ctrl.Options{
 		Scheme:             scheme,
@@ -78,14 +52,23 @@ func New(_ runtime.Object, h framework.Handle) (framework.Plugin, error) {
 		}
 	}()
 
-	scvCache := mgr.GetCache()
+	return &Yoda{
+		handle: h,
+	}, nil
+}
 
-	if scvCache.WaitForCacheSync(context.TODO()) {
-		return &Yoda{
-			handle: h,
-			cache:  scvCache,
-		}, nil
-	} else {
-		return nil, errors.New("Cache Not Sync! ")
-	}
+func (y *Yoda) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+	panic("implement me")
+}
+
+func (y *Yoda) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	panic("implement me")
+}
+
+func (y *Yoda) PreFilter(ctx context.Context, state *framework.CycleState, p *v1.Pod) *framework.Status {
+	panic("implement me")
+}
+
+func (y *Yoda) PreFilterExtensions() framework.PreFilterExtensions {
+	panic("implement me")
 }
